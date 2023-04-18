e730a934aaf76a9b41c5d0c9f34d852d095a6c9f
rodocarm <rcarmen@my.wctc.edu>
2023-04-18 12:48:20 -0500

    .NET Final Project 4/17

---
+++ Controllers/APIController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Northwind.Controllers
+{
+    public class APIController : Controller
+    {
+        // this controller depends on the NorthwindRepository
+        private DataContext _dataContext;
+        public APIController(DataContext db) => _dataContext = db;
+
+        [HttpGet, Route("api/product")]
+        // returns all products
+        public IEnumerable<Product> Get() => _dataContext.Products.OrderBy(p => p.ProductName);
+        [HttpGet, Route("api/product/{id}")]
+        // returns specific product
+        public Product Get(int id) => _dataContext.Products.FirstOrDefault(p => p.ProductId == id);
+        [HttpGet, Route("api/product/discontinued/{discontinued}")]
+        // returns all products where discontinued = true/false
+        public IEnumerable<Product> GetDiscontinued(bool discontinued) => _dataContext.Products.Where(p => p.Discontinued == discontinued).OrderBy(p => p.ProductName);
+                [HttpGet, Route("api/category/{CategoryId}/product")]
+        // returns all products in a specific category
+        public IEnumerable<Product> GetByCategory(int CategoryId) => _dataContext.Products.Where(p => p.CategoryId == CategoryId).OrderBy(p => p.ProductName);
+                [HttpGet, Route("api/category/{CategoryId}/product/discontinued/{discontinued}")]
+        // returns all products in a specific category where discontinued = true/false
+        public IEnumerable<Product> GetByCategoryDiscontinued(int CategoryId, bool discontinued) => _dataContext.Products.Where(p => p.CategoryId == CategoryId && p.Discontinued == discontinued).OrderBy(p => p.ProductName);
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class ProductController : Controller
+{
+  // this controller depends on the NorthwindRepository
+  private DataContext _dataContext;
+  public ProductController(DataContext db) => _dataContext = db;
+  public IActionResult Category() => View(_dataContext.Categories.OrderBy(c => c.CategoryName));
+  public IActionResult Index(int id){
+    ViewBag.id = id;
+    return View(_dataContext.Categories.OrderBy(c => c.CategoryName));
+  }}
